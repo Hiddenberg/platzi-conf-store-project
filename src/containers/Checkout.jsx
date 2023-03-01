de008a723a3b28946fd06f6f3fc689369041433a
Victor Pazaran <victor_hals@hotmail.com>
2023-02-28 20:12:31 -0600

    adding the chekcout page logic to remove items from the cart and the the total price of the added items

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,29 +1,46 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import AppContext from "../context/AppContext";
 import "../styles/components/Checkout.css";
 
 export default function Checkout() {
+   const {state: {cart}, removeFromCart} = useContext(AppContext);
+   
+   const totalPrice = cart.reduce((prev, curr) => prev + curr.price, 0);
+   const itemsInCart = cart.length > 0;
+
    return (
       <div className="Checkout">
          <div className="Checkout-content">
-            <h3>Lista de Pedidos:</h3>
-            <div className="Checkout-item">
-               <div className="Checkout-element">
-                  <h4>ITEM name</h4>
-                  <span>$21</span>
-               </div>
+            {itemsInCart ?
+               <h3>Lista de Pedidos:</h3> :
+               <h3>Sin articulos en el carrito</h3>
+            }
+            {itemsInCart && 
+               cart.sort((item, prevItem) => item.title > prevItem.title ? 1 : -1)
+                  .map(cartItem => (
+                     <div className="Checkout-item" key={cartItem.cartId}>
+                        <div className="Checkout-element">
+                           <h4>{cartItem.title}</h4>
+                           <span>${cartItem.price}</span>
+                        </div>
 
-               <button title="delete item" type="button">
-                  <i className="fas fa-trash-alt" />
-               </button>
+                        <button onClick={() => removeFromCart(cartItem)} title="delete item" type="button">
+                           <i className="fas fa-trash-alt" />
+                        </button>
+                     </div>
+                  ))
+            }
+         </div>
+
+         {itemsInCart &&
+            <div className="Checkout-sidebar">
+               <h3>Precio total: ${totalPrice}</h3>
+               <Link to="/checkout/information">
+                  <button type="button">Continuar pedido</button>
+               </Link>
             </div>
-         </div>
-
-         <div className="Checkout-sidebar">
-            <h3>Precio total: $21</h3>
-            <Link to="/checkout/information">
-               <button type="button">Continuar pedido</button>
-            </Link>
-         </div>
+         }
       </div>
    );
 }
--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -5,15 +5,19 @@ export default function useInitialState() {
    const [state, setState] = useState(initialState);
 
    const addToCart = payload => {
+      // Assigning first a unique cart id to be able to delete only that element in the future
+      const randomNumber = Math.floor(Math.random() * Date.now());
+      const newItem = {...payload, cartId: `${payload.id}-${randomNumber}`};
+
       setState({
          ...state,
-         cart: [...state.cart, payload]
+         cart: [...state.cart, newItem]
       });
    };
    const removeFromCart = payload => {
       setState({
          ...state,
-         cart: state.cart.filter(item => item.id !== payload.id)
+         cart: state.cart.filter(item => item.cartId !== payload.cartId)
       });
    };
 
